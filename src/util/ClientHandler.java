03196d0a4ea26410fbf1a26f020152bc65c4125f
200184800170 <tharuuwijesekara13@gmail.com>
2023-06-10 18:29:45 +0530

    Client Server Update

--- src/util/ClientHandler.java
+++ src/util/ClientHandler.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ClientHandler extends Thread {
-    private ArrayList<ClientHandler> clients;
+    public static ArrayList<ClientHandler> clients;
     private Socket socket;
     private BufferedReader reader;
     private PrintWriter writer;
@@ -19,6 +19,7 @@ public class ClientHandler extends Thread {
             this.clients = clients;
             this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.writer = new PrintWriter(socket.getOutputStream(), true);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
